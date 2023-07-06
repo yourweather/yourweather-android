aafc33d46153c26f3eec47ce2d2b808b74f6e320
Yerin Song <angelrin7@naver.com>
2023-07-07 04:26:22 +0900

    [add] : 프로젝트 초기세팅 #1

---
+++ app/src/main/java/com/umc/yourweather/data/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.umc.yourweather.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umc.yourweather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main)
+    }
+}
