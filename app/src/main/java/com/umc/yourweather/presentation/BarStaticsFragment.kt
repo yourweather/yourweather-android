6e93a1f220044cfe13be4f6197ae91db864f3549
kangyuri1114 <yuri3334@gmail.com>
2023-07-21 02:14:06 +0900

    feat : png 추가, xml 수정 (bar 구현 전)

--- app/src/main/java/com/umc/yourweather/presentation/BarStaticsFragment.kt
+++ app/src/main/java/com/umc/yourweather/presentation/BarStaticsFragment.kt
@@ -35,7 +35,7 @@ class BarStaticsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bar_statics, container, false)
+        return inflater.inflate(R.layout.fragment_bar_statics_month, container, false)
     }
 
     companion object {
