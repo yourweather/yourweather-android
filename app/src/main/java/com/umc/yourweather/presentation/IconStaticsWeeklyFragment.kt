60b65daca4a6319522e0dd23ae498b0820709374
kangyuri1114 <yuri3334@gmail.com>
2023-07-22 02:19:51 +0900

    [add] : 미입력 내역, 입력 내역 리스트 뷰 작성, 해당 두 파일 내 리사이클러뷰 적용을 위한 item xml 생성 및 구현

--- app/src/main/java/com/umc/yourweather/presentation/IconStaticsWeeklyFragment.kt
+++ app/src/main/java/com/umc/yourweather/presentation/IconStaticsWeeklyFragment.kt
@@ -1,10 +1,10 @@
 package com.umc.yourweather.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.umc.yourweather.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -31,8 +31,9 @@ class IconStaticsWeeklyFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_icon_statics_weekly, container, false)
---
+++ app/src/main/java/com/umc/yourweather/presentation/WrittenDetailListActivity.kt
@@ -0,0 +1,12 @@
+package com.umc.yourweather.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.R
+
+class WrittenDetailListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_written_detail_list)
+    }
+}
