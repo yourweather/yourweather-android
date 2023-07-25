a58e3de8f5ca4810db212f28920ca0162fdaa687
kangyuri1114 <yuri3334@gmail.com>
2023-07-26 00:53:01 +0900

    [add] fragment_bar_statics_monthly.xml 추가

---
+++ app/src/main/java/com/umc/yourweather/presentation/AnalysisActivity.kt
@@ -0,0 +1,12 @@
+package com.umc.yourweather.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.R
+
+class AnalysisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_analysis)
+    }
+}
