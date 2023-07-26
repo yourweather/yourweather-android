d61b70e162404ff28c94cb80acef039cc1f2ae1c
kangyuri1114 <yuri3334@gmail.com>
2023-07-27 02:10:53 +0900

    [add] 미입력 내역 뷰 추가

--- app/src/main/java/com/umc/yourweather/presentation/UnwrittenDetailListActivity.kt
+++ app/src/main/java/com/umc/yourweather/presentation/UnwrittenDetailListActivity.kt
@@ -1,13 +1,12 @@
-
 package com.umc.yourweather.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.umc.yourweather.R
 
-class InitialNoWeather : AppCompatActivity() {
+class UnwrittenDetailListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_initial_no_weather)
+        setContentView(R.layout.activity_unwritten_detail_list)
     }
 }
