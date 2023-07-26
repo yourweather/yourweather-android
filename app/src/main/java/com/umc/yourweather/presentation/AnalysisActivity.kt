cb715e043b6b3600fd35d40afeffdc5641df671f
kangyuri1114 <yuri3334@gmail.com>
2023-07-27 03:52:09 +0900

    [refactor] 파일명 불일치 빌드 에러 수정

--- app/src/main/java/com/umc/yourweather/presentation/AnalysisActivity.kt
+++ app/src/main/java/com/umc/yourweather/presentation/AnalysisActivity.kt
@@ -1,5 +1,6 @@
 package com.umc.yourweather.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.umc.yourweather.databinding.ActivityAnalysisBinding
@@ -10,5 +11,11 @@ class AnalysisActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAnalysisBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnBell.setOnClickListener{
+            val mIntent = Intent(this, UnwrittenDetailListActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }
     }
 }
