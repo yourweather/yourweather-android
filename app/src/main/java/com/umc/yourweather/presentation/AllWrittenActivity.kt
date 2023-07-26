d97da59466ef82d3ea88326b4a1be597e9ca89d1
kangyuri1114 <yuri3334@gmail.com>
2023-07-27 02:40:00 +0900

    [add] 모든 기록 입력 완료 창 추가

---
+++ app/src/main/java/com/umc/yourweather/presentation/AllWrittenActivity.kt
@@ -0,0 +1,12 @@
+package com.umc.yourweather.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umc.yourweather.R
+
+class AllWrittenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_written)
+    }
+}
