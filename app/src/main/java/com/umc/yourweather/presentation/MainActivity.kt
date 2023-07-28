1f764835bf6e9e707e461aa8c402d23b53348ab0
kangyuri1114 <yuri3334@gmail.com>
2023-07-29 00:27:20 +0900

    [refactor] 코드 수정

--- app/src/main/java/com/umc/yourweather/presentation/MainActivity.kt
+++ app/src/main/java/com/umc/yourweather/presentation/MainActivity.kt
@@ -2,20 +2,19 @@
 package com.umc.yourweather.presentation
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.umc.yourweather.R
+import androidx.appcompat.app.AppCompatActivity
 import com.umc.yourweather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnMain.setOnClickListener{
-            val mIntent = Intent(this, AnalysisActivity::class.java)
+        binding.btnMain.setOnClickListener {
+            val mIntent = Intent(this, SignIn::class.java)
             startActivity(mIntent)
             finish()
         }
