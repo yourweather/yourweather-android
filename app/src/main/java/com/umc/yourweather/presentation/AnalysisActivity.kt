4915d4bc8bb8711049ab0fd3f5e801e390552529
kangyuri1114 <yuri3334@gmail.com>
2023-07-28 21:04:59 +0900

    [feat] 비교분석 뷰 버튼 클릭 기능 구현 중

--- app/src/main/java/com/umc/yourweather/presentation/AnalysisActivity.kt
+++ app/src/main/java/com/umc/yourweather/presentation/AnalysisActivity.kt
@@ -1,18 +1,47 @@
 package com.umc.yourweather.presentation
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.R.drawable
 import com.umc.yourweather.databinding.ActivityAnalysisBinding
 
+@Suppress("DEPRECATION")
 class AnalysisActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAnalysisBinding
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAnalysisBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnBell.setOnClickListener{
+        val normalBackground: Drawable = resources.getDrawable(drawable.btn_transp_select_rec)
+        val pressedBackground: Drawable = resources.getDrawable(drawable.btn_brown_select_rec)
+
+        binding.btnAnalysisMonthly.setOnClickListener {
+            if (binding.btnAnalysisMonthly.background === normalBackground) {
+                binding.btnAnalysisMonthly.background = pressedBackground
+                binding.btnAnalysisWeekly.background = normalBackground
+            } else {
+                binding.btnAnalysisMonthly.background = normalBackground
+                binding.btnAnalysisWeekly.background = pressedBackground
+            }
+        }
+
+        binding.btnAnalysisWeekly.setOnClickListener {
+            if (binding.btnAnalysisWeekly.background === normalBackground) {
+                binding.btnAnalysisWeekly.background = pressedBackground
+                binding.btnAnalysisMonthly.background = normalBackground
+            } else {
+                binding.btnAnalysisWeekly.background = normalBackground
+                binding.btnAnalysisMonthly.background = pressedBackground
+            }
+        }
+
+        binding.btnBell.setOnClickListener {
             val mIntent = Intent(this, UnwrittenDetailListActivity::class.java)
             startActivity(mIntent)
             finish()
