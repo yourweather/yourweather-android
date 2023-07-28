406cd2232c957f96e218e1693fb32ad0285f9afa
Parkseojin <seojin5565@gmail.com>
2023-07-29 02:10:31 +0900

    feat : acvitiy_signup2에 비밀번호 재확인 로직 구현 #53

--- app/src/main/java/com/umc/yourweather/presentation/SignIn.kt
+++ app/src/main/java/com/umc/yourweather/presentation/SignIn.kt
@@ -3,9 +3,15 @@ package com.umc.yourweather.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.WindowManager
+import android.widget.TextView
+import android.widget.Toast
 import com.umc.yourweather.R
 import com.umc.yourweather.databinding.ActivitySignInBinding
 import com.umc.yourweather.databinding.ActivitySignUpBinding
+import com.umc.yourweather.util.CalendarUtils.Companion.dpToPx
 
 class SignIn : AppCompatActivity() {
     lateinit var binding : ActivitySignInBinding
@@ -21,9 +27,46 @@ class SignIn : AppCompatActivity() {
         }
 
         binding.tvSigninBtnsignup.setOnClickListener{
+            customToast()
             val mIntent = Intent(this, SignUp::class.java)
             startActivity(mIntent)
         }
+    }
+    fun customToast(){
+        val inflater = LayoutInflater.from(this)
+        val layout = inflater.inflate(R.layout.toast_signin, findViewById(R.id.ll_signin_toast), false)
 
+        val textViewMessage = layout.findViewById<TextView>(R.id.tv_signin_toast)
+        textViewMessage.text = "이메일 또는 비밀번호를 다시 입력해주세요"
+        val toast = Toast(applicationContext)
+
+        val params = WindowManager.LayoutParams()
+        params.width = dpToPx(this@SignIn, 297).toInt()
+        params.height = dpToPx(this@SignIn, 40).toInt()
+        params.gravity = Gravity.CENTER
+
+        toast.view?.layoutParams = params
+        toast.duration = Toast.LENGTH_SHORT //메시지 표시 시간
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.view = layout
+        toast.show()
+
+//        val inflater = LayoutInflater.from(this)
+//        val layout = inflater.inflate(R.layout.toast_signin, null)
+//
+//        val popupWindow = PopupWindow(
+//            layout,
+//        )
+//
+//        val textViewMessage = layout.findViewById<TextView>(R.id.tv_signin_toast)
+//        textViewMessage.text = "이메일 또는 비밀번호를 다시 입력해주세요"
+//
+//        // 원하는 위치에 팝업 표시
+//        popupWindow.showAtLocation(binding.root, Gravity.CENTER, 0, 0)
+//
+//        // 원하는 시간만큼 팝업 유지
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            popupWindow.dismiss()
+//        }, 2000)
     }
 }
--- app/src/main/java/com/umc/yourweather/presentation/SignUp2.kt
+++ app/src/main/java/com/umc/yourweather/presentation/SignUp2.kt
@@ -1,11 +1,14 @@
 package com.umc.yourweather.presentation
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import com.umc.yourweather.R
 import com.umc.yourweather.databinding.ActivitySignUp2Binding
-import com.umc.yourweather.databinding.ActivitySignUpBinding
 
 class SignUp2 : AppCompatActivity() {
     lateinit var binding : ActivitySignUp2Binding
@@ -15,10 +18,58 @@ class SignUp2 : AppCompatActivity() {
         binding = ActivitySignUp2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSignup2Signup.setOnClickListener{
+        binding.btnSignup2Next.setOnClickListener{
             val mIntent = Intent(this, Nickname::class.java)
             startActivity(mIntent)
             finish()
+        }
+
+        binding.etSignup2Repw.addTextChangedListener(createTextWatcher())
+        binding.etSignup2Repw.addTextChangedListener(createTextWatcher())
+
+    }
+
+    private fun createTextWatcher(): TextWatcher {
+        return object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                checkError()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //입력하기 전
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //텍스트 변화가 있을 시
+                checkError()
+            }
+        }
+    }
+    private fun checkError(){
+        var pw0 = binding.etSignup2Pw.text.toString()
+        var pw1 = binding.etSignup2Repw.text.toString()
+        if(pw0.equals(pw1)){
+            binding.etSignup2Pw.background =  resources.getDrawable(R.drawable.bg_gray_ed_fill_6_rect)
+            binding.etSignup2Repw.background = resources.getDrawable(R.drawable.bg_gray_ed_fill_6_rect)
+
+            binding.tvSignup2Error.setTextColor(Color.parseColor("#99808080"))
+
+            binding.ivSignup2Check0.visibility = View.VISIBLE
+            binding.ivSignup2Check1.visibility = View.VISIBLE
+
+            binding.btnSignup2Next.isEnabled = true
+
+        }
+        else{
+            binding.etSignup2Pw.background =  resources.getDrawable(R.drawable.bg_gray_ed_fill_6_rect_border_red)
+            binding.etSignup2Repw.background = resources.getDrawable(R.drawable.bg_gray_ed_fill_6_rect_border_red)
+
+            binding.tvSignup2Error.setTextColor(Color.parseColor("#CB443D"))
+
+            binding.ivSignup2Check0.visibility = View.INVISIBLE
+            binding.ivSignup2Check1.visibility = View.INVISIBLE
+
+            binding.btnSignup2Next.isEnabled = false
         }
     }
 }
