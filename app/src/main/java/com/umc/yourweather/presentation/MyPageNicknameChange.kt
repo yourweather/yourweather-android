8bd9bb7dee401f8d640a8512e5974873ac9c1c0a
Parkseojin <seojin5565@gmail.com>
2023-07-30 21:49:19 +0900

    [add] : 마이페이지 닉네임 변경 뷰 추가 #86

---
+++ app/src/main/java/com/umc/yourweather/presentation/MyPageNicknameChange.kt
@@ -0,0 +1,18 @@
+package com.umc.yourweather.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umc.yourweather.R
+import com.umc.yourweather.databinding.ActivityMyPageNicknameChangeBinding
+
+class MyPageNicknameChange : AppCompatActivity() {
+    lateinit var binding: ActivityMyPageNicknameChangeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyPageNicknameChangeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
