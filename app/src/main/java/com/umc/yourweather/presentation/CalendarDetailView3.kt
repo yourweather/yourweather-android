cfb296f3958b8f9fe33f2ab98498c6bb7cf96df9
hyemyu <hyemione17@gmail.com>
2023-07-30 17:00:33 +0900

    [add] 캘린더 상세보기 뷰 구현 #24

---
+++ app/src/main/java/com/umc/yourweather/presentation/CalendarDetailView3.kt
@@ -0,0 +1,12 @@
+package com.umc.yourweather.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umc.yourweather.R
+
+class CalendarDetailView3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar_detail_view3)
+    }
+}
