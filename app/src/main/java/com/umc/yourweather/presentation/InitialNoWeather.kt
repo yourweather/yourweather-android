265962156c65a0c3d9a40cddd573756784628fc1
Yerin Song <angelrin7@naver.com>
2023-07-30 22:45:53 +0900

    [feat] : 토스트 기능 로직 추가

--- app/src/main/java/com/umc/yourweather/presentation/InitialNoWeather.kt
+++ app/src/main/java/com/umc/yourweather/presentation/InitialNoWeather.kt
@@ -2,6 +2,8 @@
 package com.umc.yourweather.presentation
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.umc.yourweather.R
 
@@ -9,5 +11,14 @@ class InitialNoWeather : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial_no_weather)
+
+        val customToastView = LayoutInflater.from(this).inflate(R.layout.toast_initial, null)
+
+        val initialToast = Toast(this)
+        initialToast.view = customToastView
+
+        initialToast.duration = Toast.LENGTH_LONG
+
+        initialToast.show()
     }
 }
