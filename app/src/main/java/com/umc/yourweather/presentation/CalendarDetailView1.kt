1d2ed18591511e2954fb031f42f8a057db9100e5
Parkseojin <seojin5565@gmail.com>
2023-08-01 00:34:31 +0900

    [feat] : 캘린더-캘린더 디테일 뷰 연결 #48

--- app/src/main/java/com/umc/yourweather/presentation/CalendarDetailView1.kt
+++ app/src/main/java/com/umc/yourweather/presentation/CalendarDetailView1.kt
@@ -1,6 +1,5 @@
 package com.umc.yourweather.presentation
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -12,29 +11,28 @@ import com.umc.yourweather.R
 class CalendarDetailView1 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar_detail_view1)
+
         val textView1: TextView = findViewById(R.id.textView)
         val textView3: TextView = findViewById(R.id.textView3) // 선언
         val year = intent.getStringExtra("year")
         val month = intent.getStringExtra("month")
         val date = intent.getStringExtra("date")
 
-
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calendar_detail_view1)
-
-        val btnBack : ImageButton = findViewById(R.id.imageButton)
+        val btnBack: ImageButton = findViewById(R.id.imageButton)
 
         btnBack.setOnClickListener {
             finish()
-
         }
+
         textView1.text = "${month}월 ${date}일 이다은님의 날씨"
         textView3.text = "${month}월 ${date}일의 일기"
 
-        val btnModify : Button = findViewById(R.id.button)
+        val btnModify: Button = findViewById(R.id.button)
 
         btnModify.setOnClickListener {
-            val intent= Intent(this@CalendarDetailView1,CalendarDetailView3::class.java)
+            val intent = Intent(this@CalendarDetailView1, CalendarDetailView3::class.java)
             startActivity(intent)
         }
     }
