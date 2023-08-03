99c0cadec645ca22ebc80035636b6024acd49fa5
kangyuri1114 <yuri3334@gmail.com>
2023-08-03 19:25:31 +0900

    [feat] : 주간 bar 클릭 시 말풍선 구현

--- app/src/main/java/com/umc/yourweather/presentation/analysis/BarStaticsWeeklyFragment.kt
+++ app/src/main/java/com/umc/yourweather/presentation/analysis/BarStaticsWeeklyFragment.kt
@@ -2,6 +2,7 @@ package com.umc.yourweather.presentation.analysis
 
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
@@ -62,15 +63,15 @@ class BarStaticsWeeklyFragment : Fragment() {
         val dataList = listOf(
             BarData("맑음", 44),
             BarData("흐림", 60),
-            BarData("번개", 100),
             BarData("비", 24),
-        )
+            BarData("번개", 100),
+            )
 
         val dataList2 = listOf(
             BarData("맑음", 100),
             BarData("흐림", 34),
-            BarData("번개", 20),
             BarData("비", 66),
+            BarData("번개", 20),
         )
 
         // 각 데이터 값에 해당하는 너비 계산
@@ -91,13 +92,18 @@ class BarStaticsWeeklyFragment : Fragment() {
             val drawableRes = when (data.label) {
                 "맑음" -> R.drawable.bg_yellow_rec_round_sun
                 "흐림" -> R.drawable.bg_gray_rec_cloud
-                "번개" -> R.drawable.bg_blue_rec_rain
-                "비" -> R.drawable.bg_darkblue_rec_round_thunder
+                "비" -> R.drawable.bg_blue_rec_rain
+                "번개" -> R.drawable.bg_darkblue_rec_round_thunder
                 else -> R.drawable.bg_gray_rec_cloud
             }
 
             view.background = ContextCompat.getDrawable(requireContext(), drawableRes)
             binding.llAnalysisBarLastWeek.addView(view)
+
+            // 클릭 시 말풍선 보이게
+            view.setOnClickListener {
+                showBallViewLastWeek(data.label)
+            }
         }
 
         for (data in dataList2) {
@@ -115,16 +121,81 @@ class BarStaticsWeeklyFragment : Fragment() {
             val drawableRes = when (data.label) {
                 "맑음" -> R.drawable.bg_yellow_rec_round_sun
                 "흐림" -> R.drawable.bg_gray_rec_cloud
-                "번개" -> R.drawable.bg_blue_rec_rain
-                "비" -> R.drawable.bg_darkblue_rec_round_thunder
+                "비" -> R.drawable.bg_blue_rec_rain
+                "번개" -> R.drawable.bg_darkblue_rec_round_thunder
                 else -> R.drawable.bg_gray_rec_cloud
             }
 
             view.background = ContextCompat.getDrawable(requireContext(), drawableRes)
             binding.llAnalysisBarThisWeek.addView(view)
+
+            // 클릭 시 말풍선 보이게
+            view.setOnClickListener {
+                showBallViewThisWeek(data.label)
+            }
+        }
+    }
+    private fun showBallViewLastWeek(weatherLabel: String) {
+        // 모든 텍스트뷰 숨기기
+        binding.tvBalloonSunLastWeek.visibility = View.GONE
+        binding.tvBalloonRainLastWeek.visibility = View.GONE
+        binding.tvBalloonCloudLastWeek.visibility = View.GONE
+        binding.tvBalloonThunderLastWeek.visibility = View.GONE
+
+        // 선택한 날씨에 맞는 TextView를 보이도록 설정
+        when (weatherLabel) {
+            "맑음" -> {
+                binding.tvBalloonSunLastWeek.visibility = View.VISIBLE
+                hideBallViewAfterDelay(binding.tvBalloonSunLastWeek)
+            }
+            "흐림" -> {
+                binding.tvBalloonCloudLastWeek.visibility = View.VISIBLE
+                hideBallViewAfterDelay(binding.tvBalloonCloudLastWeek)
+            }
+            "번개" -> {
+                binding.tvBalloonThunderLastWeek.visibility = View.VISIBLE
+                hideBallViewAfterDelay(binding.tvBalloonThunderLastWeek)
+            }
+            "비" -> {
+                binding.tvBalloonRainLastWeek.visibility = View.VISIBLE
+                hideBallViewAfterDelay(binding.tvBalloonRainLastWeek)
+            }
         }
     }
 
+    private fun showBallViewThisWeek(weatherLabel: String) {
+        // 모든 텍스트뷰 숨기기
+        binding.tvBalloonSunThisWeek.visibility = View.GONE
+        binding.tvBalloonRainThisWeek.visibility = View.GONE
+        binding.tvBalloonCloudThisWeek.visibility = View.GONE
+        binding.tvBalloonThunderThisWeek.visibility = View.GONE
+
+        // 선택한 날씨에 맞는 TextView를 보이도록 설정
+        when (weatherLabel) {
+            "맑음" -> {
+                binding.tvBalloonSunThisWeek.visibility = View.VISIBLE
+                hideBallViewAfterDelay(binding.tvBalloonSunThisWeek)
+            }
+            "흐림" -> {
+                binding.tvBalloonCloudThisWeek.visibility = View.VISIBLE
+                hideBallViewAfterDelay(binding.tvBalloonCloudThisWeek)
+            }
+            "번개" -> {
+                binding.tvBalloonThunderThisWeek.visibility = View.VISIBLE
+                hideBallViewAfterDelay(binding.tvBalloonThunderThisWeek)
+            }
+            "비" -> {
+                binding.tvBalloonRainThisWeek.visibility = View.VISIBLE
+                hideBallViewAfterDelay(binding.tvBalloonRainThisWeek)
+            }
+        }
+    }
+    private fun hideBallViewAfterDelay(view: View) {
+        val handler = Handler()
+        handler.postDelayed({
+            view.visibility = View.GONE
+        }, 1500) // 1.5초 후에 말풍선을 숨김
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
