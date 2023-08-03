f0bdd7aa1883ecef9f58044086941d77fb13ad31
Yerin Song <angelrin7@naver.com>
2023-08-03 14:21:36 +0900

    [fix] : initial뷰 토스트메시지 생성 오류 해결

--- app/src/main/java/com/umc/yourweather/presentation/InitialNoWeatherFragment.kt
+++ app/src/main/java/com/umc/yourweather/presentation/InitialNoWeatherFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.umc.yourweather.R
 import com.umc.yourweather.databinding.FragmentInitialNoWeatherBinding
@@ -24,6 +25,8 @@ class InitialNoWeatherFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        showInitialToast()
+
         binding.btnInitialWeather.setOnClickListener {
             val newFragment = HomeWeatherInputFragment()
             val transaction = parentFragmentManager.beginTransaction()
@@ -31,5 +34,12 @@ class InitialNoWeatherFragment : Fragment() {
             transaction.addToBackStack(null)
             transaction.commit()
         }
+    }
+
+    private fun showInitialToast() {
+        val toastView = layoutInflater.inflate(R.layout.toast_initial, binding.root, false)
+        val toast = Toast(context)
+        toast.view = toastView
+        toast.show()
     }
 }
