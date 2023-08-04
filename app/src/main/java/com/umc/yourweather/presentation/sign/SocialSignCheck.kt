6680c326dae6083aacb1b130b39ca7d5b55ca38b
Parkseojin <seojin5565@gmail.com>
2023-08-04 21:16:13 +0900

    [add] : 소셜로그인 테스트용 액티비티 파일 추가(제거예정) #17

---
+++ app/src/main/java/com/umc/yourweather/presentation/sign/SocialSignCheck.kt
@@ -0,0 +1,12 @@
+package com.umc.yourweather.presentation.sign
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.R
+
+class SocialSignCheck : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_social_sign_check)
+    }
+}
