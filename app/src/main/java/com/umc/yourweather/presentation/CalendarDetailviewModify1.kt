e7921d65c7aa20b9dec50b50c45030a66e5955e0
hyemyu <hyemione17@gmail.com>
2023-08-06 00:11:03 +0900

    add : 캘린더 상세보기 버튼 눌렀을 경우 뷰 구현 #159

---
+++ app/src/main/java/com/umc/yourweather/presentation/CalendarDetailviewModify1.kt
@@ -0,0 +1,16 @@
+package com.umc.yourweather.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.umc.yourweather.R
+import com.umc.yourweather.databinding.ActivityCalendarDetailviewModify1Binding
+
+class CalendarDetailviewModify1 : AppCompatActivity() {
+
+}
