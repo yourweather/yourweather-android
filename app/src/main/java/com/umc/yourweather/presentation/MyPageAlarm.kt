44b80dd11b6c94895cdebde2f68e08a7f8016d1e
kangyuri1114 <yuri3334@gmail.com>
2023-08-07 03:17:08 +0900

    [fix] : 스위치 커스텀 완료 -> 마이페이지 알람 화면 구현 완료

--- app/src/main/java/com/umc/yourweather/presentation/MyPageAlarm.kt
+++ app/src/main/java/com/umc/yourweather/presentation/MyPageAlarm.kt
@@ -1,10 +1,12 @@
 package com.umc.yourweather.presentation
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umc.yourweather.R
 
 class MyPageAlarm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page_alarm)
     }
 }
