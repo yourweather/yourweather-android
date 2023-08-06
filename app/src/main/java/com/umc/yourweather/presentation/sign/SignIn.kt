40c43f4bf428ac0e7eeea1736814c4babce83253
ParkSeojin1101 <109809242+ParkSeojin1101@users.noreply.github.com>
2023-08-06 22:16:09 +0900

    [setting/#163]레트로핏 의존성 추가, base url 추가 (#164)
    * [add] : 소셜로그인 테스트용 액티비티 파일 추가(제거예정) #17
    
    * [refactor] : 필요없는 코드 삭제 #17
    
    * [setting] : 카카오로그인 의존성 추가 #17
    
    * [setting] : 레트로핏 세팅 #163
    
    * [setting] : 인터넷권한 세팅 #163

--- app/src/main/java/com/umc/yourweather/presentation/sign/SignIn.kt
+++ app/src/main/java/com/umc/yourweather/presentation/sign/SignIn.kt
@@ -2,12 +2,14 @@ package com.umc.yourweather.presentation.sign
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.WindowManager
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.BuildConfig
 import com.umc.yourweather.R
 import com.umc.yourweather.databinding.ActivitySignInBinding
 import com.umc.yourweather.presentation.BottomNavi
@@ -21,25 +23,32 @@ class SignIn : AppCompatActivity() {
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 비밀번호 찾기로 이동
         binding.tvSigninBtnfindpw.setOnClickListener {
             val mIntent = Intent(this, FindPw::class.java)
             startActivity(mIntent)
         }
 
+        // 회원가입으로 이동
         binding.tvSigninBtnsignup.setOnClickListener {
             // customToast()
             val mIntent = Intent(this, SignUp::class.java)
             startActivity(mIntent)
         }
 
+        // 로그인 버튼 클릭
         binding.btnSigninSignin.setOnClickListener {
             val mIntent = Intent(this, BottomNavi::class.java)
             startActivity(mIntent)
         }
+
+        Log.d("base url", "${BuildConfig.BASE_URL}")
     }
+
     fun customToast() {
         val inflater = LayoutInflater.from(this)
-        val layout = inflater.inflate(R.layout.toast_signin, findViewById(R.id.ll_signin_toast), false)
+        val layout =
+            inflater.inflate(R.layout.toast_signin, findViewById(R.id.ll_signin_toast), false)
 
         val textViewMessage = layout.findViewById<TextView>(R.id.tv_signin_toast)
         textViewMessage.text = "이메일 또는 비밀번호를 다시 입력해주세요"
@@ -55,23 +64,5 @@ class SignIn : AppCompatActivity() {
         toast.setGravity(Gravity.CENTER, 0, 0)
         toast.view = layout
         toast.show()
-
-//        val inflater = LayoutInflater.from(this)
-//        val layout = inflater.inflate(R.layout.toast_signin, null)
-//
-//        val popupWindow = PopupWindow(
-//            layout,
-//        )
-//
-//        val textViewMessage = layout.findViewById<TextView>(R.id.tv_signin_toast)
-//        textViewMessage.text = "이메일 또는 비밀번호를 다시 입력해주세요"
-//
-//        // 원하는 위치에 팝업 표시
-//        popupWindow.showAtLocation(binding.root, Gravity.CENTER, 0, 0)
-//
-//        // 원하는 시간만큼 팝업 유지
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            popupWindow.dismiss()
-//        }, 2000)
     }
 }
