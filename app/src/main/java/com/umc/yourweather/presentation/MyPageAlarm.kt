f0606ec59166662ebc23d1606e430a54b1c526ba
kangyuri1114 <yuri3334@gmail.com>
2023-08-07 02:39:42 +0900

    [add] : 마이페이지 알람 액티비티 추가

---
+++ app/src/main/java/com/umc/yourweather/presentation/MyPageAlarm.kt
@@ -0,0 +1,10 @@
+package com.umc.yourweather.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyPageAlarm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
