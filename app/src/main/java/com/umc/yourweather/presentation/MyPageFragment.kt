bb20b0654c67dcdefc9d43e928632fe3d6bb0e43
Yerin Song <angelrin7@naver.com>
2023-08-07 04:53:34 +0900

    [add] : 마이페이지 첫 화면 프래그먼트 생성

---
+++ app/src/main/java/com/umc/yourweather/presentation/MyPageFragment.kt
@@ -0,0 +1,19 @@
+package com.umc.yourweather.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.umc.yourweather.R
+
+
+class MyPageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    }
+}
