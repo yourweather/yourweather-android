c6533cb2d507ece4603873998438225aac53a5a1
Yerin Song <angelrin7@naver.com>
2023-08-07 16:37:05 +0900

    [add] : userservice 인터페이스 생성

--- app/src/main/java/com/umc/yourweather/remote/response/BaseResponse.kt
+++ app/src/main/java/com/umc/yourweather/remote/response/BaseResponse.kt
@@ -1,6 +1,5 @@
 package com.umc.yourweather.remote.response
 
-
 data class BaseResponse<T>(
     val success: Boolean,
     val code: Int,
---
+++ app/src/main/java/com/umc/yourweather/service/UserService.kt
@@ -0,0 +1,4 @@
+package com.umc.yourweather.service
+
+interface UserService {
+}
