aefe67f6096d5a2cfd4f6dcbc002b91fa8ab60a6
kangyuri1114 <yuri3334@gmail.com>
2023-08-09 00:45:05 +0900

    [feat] : 뒤로가기 구현

--- app/src/main/java/com/umc/yourweather/presentation/mypage/MyPageAlarm.kt
+++ app/src/main/java/com/umc/yourweather/presentation/mypage/MyPageAlarm.kt
@@ -1,12 +1,18 @@
 package com.umc.yourweather.presentation.mypage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.umc.yourweather.R
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.databinding.ActivityMyPageAlarmBinding
 
 class MyPageAlarm : AppCompatActivity() {
+    private lateinit var binding: ActivityMyPageAlarmBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_page_alarm)
+        binding = ActivityMyPageAlarmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMyAlarm.setOnClickListener {
+            finish()
+        }
     }
 }
