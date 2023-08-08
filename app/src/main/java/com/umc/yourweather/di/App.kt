c7f8d9e423c018a706876cc48174216e8389dcac
Yerin Song <angelrin7@naver.com>
2023-08-08 23:50:09 +0900

    [add] 레트로핏 세팅 파일 추가

---
+++ app/src/main/java/com/umc/yourweather/di/App.kt
@@ -0,0 +1,21 @@
+package com.umc.yourweather.di
+
+import android.app.Application
+import android.content.Context
+import com.kakao.sdk.common.KakaoSdk
+import com.umc.yourweather.BuildConfig
+
+class App : Application() {
+    companion object {
+        var appContext: Context? = null
+        lateinit var token_prefs: TokenSharedPreferences
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        token_prefs = TokenSharedPreferences(applicationContext)
+
+        appContext = this
+        KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
+    }
+}
