21fe9db7acbecc8c271b5dd87ab66e7a130fc998
Parkseojin <seojin5565@gmail.com>
2023-08-08 16:01:07 +0900

    [feat] : 마이페이지 -> 내정보 이동 로직 추가  #180

--- app/src/main/java/com/umc/yourweather/presentation/MyPageFragment.kt
+++ app/src/main/java/com/umc/yourweather/presentation/MyPageFragment.kt
@@ -1,19 +1,28 @@
 package com.umc.yourweather.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.umc.yourweather.R
-
+import com.umc.yourweather.databinding.FragmentMyPageBinding
 
 class MyPageFragment : Fragment() {
+    lateinit var binding: FragmentMyPageBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+        binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivMyPageNextInfo.setOnClickListener {
+            val mIntent = Intent(activity, MyPageMyInfo::class.java)
+            startActivity(mIntent)
+        }
     }
 }
