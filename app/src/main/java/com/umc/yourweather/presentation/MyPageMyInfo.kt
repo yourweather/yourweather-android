a84146f27784dbf9e6c3a2ad3644eb77fae5c965
Parkseojin <seojin5565@gmail.com>
2023-08-08 15:53:40 +0900

    [feat] : 내 정보 페이지 이동 로직 추가  #180

--- app/src/main/java/com/umc/yourweather/presentation/MyPageMyInfo.kt
+++ app/src/main/java/com/umc/yourweather/presentation/MyPageMyInfo.kt
@@ -1,10 +1,29 @@
 package com.umc.yourweather.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.databinding.ActivityMyInfoBinding
 
 class MyPageMyInfo : AppCompatActivity() {
+    lateinit var binding: ActivityMyInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMyInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.flMyinfoBackbtn.setOnClickListener {
+            finish()
+        }
+
+        binding.btnMyinfoPwChange.setOnClickListener {
+            val mIntent = Intent(this, MyPagePwChange::class.java)
+            startActivity(mIntent)
+        }
+
+        binding.btnMyinfoNicknameChange.setOnClickListener {
+            val mIntent = Intent(this, MyPageNicknameChange::class.java)
+            startActivity(mIntent)
+        }
     }
 }
