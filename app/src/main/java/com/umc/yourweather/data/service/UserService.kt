4be4565ca05c307d2fa43b601f3e98f54f4d5d9b
Yerin Song <angelrin7@naver.com>
2023-08-09 17:20:42 +0900

    [feat] : userservice 내용 추가

--- app/src/main/java/com/umc/yourweather/data/service/UserService.kt
+++ app/src/main/java/com/umc/yourweather/data/service/UserService.kt
@@ -1,4 +1,30 @@
 package com.umc.yourweather.data.service
 
+import com.umc.yourweather.data.remote.request.ChangePasswordRequest
+import com.umc.yourweather.data.remote.request.SignupRequest
+import com.umc.yourweather.data.remote.response.BaseResponse
+import com.umc.yourweather.data.remote.response.UserResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+
 interface UserService {
+
+    // 회원 탈퇴
+    @PUT("/api/v1/users/withdraw")
+    fun withdrawUser(): Call<BaseResponse<UserResponse>>
+
+    // 회원 가입
+    @POST("/api/v1/users/signup")
+    fun signUp(@Body request: SignupRequest): Call<BaseResponse<String>>
+
+    // 비밀번호 변경
+    @POST("/api/v1/users/password")
+    fun changePw(@Body request: ChangePasswordRequest): Call<BaseResponse<UserResponse>>
+
+    // 마이페이지
+    @GET("/api/v1/users/mypage")
+    fun getMyPage(): Call<BaseResponse<UserResponse>>
 }
