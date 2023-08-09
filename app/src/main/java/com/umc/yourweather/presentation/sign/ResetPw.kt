368c207370a2a08e94219afe84d7a37529f4a619
kangyuri1114 <yuri3334@gmail.com>
2023-08-10 03:14:35 +0900

    [add] : 비밀번호 재설정 액티비티 파일 생성

---
+++ app/src/main/java/com/umc/yourweather/presentation/sign/ResetPw.kt
@@ -0,0 +1,12 @@
+package com.umc.yourweather.presentation.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umc.yourweather.R
+
+class ResetPw : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pw_reset)
+    }
+}
