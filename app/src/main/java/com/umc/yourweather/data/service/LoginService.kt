82e22327946501b321dbfcf5f3f23ad011009193
Parkseojin <seojin5565@gmail.com>
2023-08-09 17:15:43 +0900

    [add] : LoginService.kt 파일 추가 #207

--- app/src/main/java/com/umc/yourweather/data/service/LoginService.kt
+++ app/src/main/java/com/umc/yourweather/data/service/LoginService.kt
@@ -1,4 +1,18 @@
 package com.umc.yourweather.data.service
 
+import com.umc.yourweather.data.remote.request.LoginRequset
+import com.umc.yourweather.data.remote.response.TokenResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface LoginService {
+
+    // 일반 로그인
+    @POST("api/v1/users/login")
+    fun logIn(@Body request: LoginRequset): Call<TokenResponse>
+
+    // 소셜 로그인
+    @POST("api/v1/users/oauth-login")
+    fun oauthLogIn(@Body request: LoginRequset): Call<TokenResponse>
 }
