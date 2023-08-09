de2c2316f6a226fe66d99c0618aec55cead3940f
kangyuri1114 <yuri3334@gmail.com>
2023-08-10 03:37:15 +0900

    [feat] : 기본 뷰 연결

--- app/src/main/java/com/umc/yourweather/presentation/sign/ResetPw.kt
+++ app/src/main/java/com/umc/yourweather/presentation/sign/ResetPw.kt
@@ -2,11 +2,14 @@ package com.umc.yourweather.presentation.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.umc.yourweather.R
+import com.umc.yourweather.databinding.ActivityPwResetBinding
 
 class ResetPw : AppCompatActivity() {
+    lateinit var binding: ActivityPwResetBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pw_reset)
+        binding = ActivityPwResetBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
