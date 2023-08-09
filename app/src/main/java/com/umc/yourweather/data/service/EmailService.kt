47cd2105b8ddcf5e3024859922b58b7715d3866e
Yerin Song <angelrin7@naver.com>
2023-08-09 17:42:12 +0900

    [feat] : EmailService 내용 추가

--- app/src/main/java/com/umc/yourweather/data/service/EmailService.kt
+++ app/src/main/java/com/umc/yourweather/data/service/EmailService.kt
@@ -1,4 +1,22 @@
 package com.umc.yourweather.data.service
 
+import com.umc.yourweather.data.remote.response.BaseResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface EmailService {
+
+    // 이메일 전송
+    @GET("/api/v1/email/send")
+    fun sendEmail(
+        @Query("email") email: String,
+    ): Call<BaseResponse<Unit>>
+
+    // 이메일 인증
+    @GET("/api/v1/email/certify")
+    fun certifyEmail(
+        @Query("email") email: String,
+        @Query("code") code: String,
+    ): Call<BaseResponse<Boolean>>
 }
