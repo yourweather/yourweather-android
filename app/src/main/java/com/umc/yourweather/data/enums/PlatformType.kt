13436b79098dac7621cde54ae749451c8df81d02
Yerin Song <angelrin7@naver.com>
2023-08-10 15:07:19 +0900

    [add] : 플랫폼타입 enum class 생성

---
+++ app/src/main/java/com/umc/yourweather/data/enums/PlatformType.kt
@@ -0,0 +1,8 @@
+package com.umc.yourweather.data.enums
+
+enum class PlatformType (val displayName: String) {
+        KAKAO("카카오"),
+        GOOGLE("구글"),
+        NAVER("네이버"),
+        YOURWEATHER("유어웨더"),
+}
