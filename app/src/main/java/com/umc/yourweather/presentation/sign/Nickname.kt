dd22e1908efe61e271b2fb3b4562dfa196e27447
Yerin Song <angelrin7@naver.com>
2023-08-10 15:02:49 +0900

    [refactor] : api 관련 로직 변경

--- app/src/main/java/com/umc/yourweather/presentation/sign/Nickname.kt
+++ app/src/main/java/com/umc/yourweather/presentation/sign/Nickname.kt
@@ -1,19 +1,14 @@
 package com.umc.yourweather.presentation.sign
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.umc.yourweather.R
 import com.umc.yourweather.data.remote.request.SignupRequest
 import com.umc.yourweather.data.remote.response.BaseResponse
 import com.umc.yourweather.data.service.UserService
 import com.umc.yourweather.databinding.ActivityNicknameBinding
 import com.umc.yourweather.di.App
 import com.umc.yourweather.di.RetrofitImpl
-import com.umc.yourweather.presentation.BottomNavi
 import com.umc.yourweather.util.NicknameUtils.Companion.getRandomHintText
 import retrofit2.Call
 import retrofit2.Callback
@@ -27,8 +22,8 @@ class Nickname : AppCompatActivity() {
         binding = ActivityNicknameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val editText: EditText = findViewById(R.id.et_nickname_nickname)
-        val button: Button = findViewById(R.id.btn_nickname_refresh)
+        val editText = binding.etNicknameNickname
+        val button = binding.btnNicknameRefresh
 
         // btn 클릭할 때마다 랜덤 닉네임 추천
         button.setOnClickListener {
@@ -47,23 +42,27 @@ class Nickname : AppCompatActivity() {
             }
 
             App.globalNickname = fixedNickname // 전역 닉네임 변수에 값을 설정
-            val mIntent = Intent(this, BottomNavi::class.java)
-            startActivity(mIntent)
+            User() // 회원 가입 API 호출
         }
-        User()
     }
 
     private fun User() {
         val pw = intent.getStringExtra("password") ?: ""
         val email = intent.getStringExtra("email") ?: ""
-        Log.d("pwDebug2", "pw value2: $pw")
-        Log.d("EmailDebug3", "Email value3: $email")
         val platform = intent.getStringExtra("platform") ?: ""
-        val fixedNickname = App.globalNickname ?: ""
 
-        // 회원 가입 API 호출
+        val enteredNickname = binding.etNicknameNickname.text.toString() // 사용자가 입력한 닉네임
+        val hintNickname = binding.etNicknameNickname.hint.toString() // hint로 설정된 닉네임
+
+        val fixedNickname = if (enteredNickname.isBlank()) {
+            hintNickname
+        } else {
+            enteredNickname
+        }
+
         val signupRequest = SignupRequest(email, pw, fixedNickname, platform)
         val signupService = RetrofitImpl.nonRetrofit.create(UserService::class.java)
+
         signupService.signUp(signupRequest).enqueue(object : Callback<BaseResponse<String>> {
             override fun onResponse(
                 call: Call<BaseResponse<String>>,
@@ -74,7 +73,7 @@ class Nickname : AppCompatActivity() {
                     if (code == 200) {
                         // 회원 가입 성공
                         Log.d("SignupDebug", "회원 가입 성공")
-                        
+                        // 원하는 처리 작업 수행
                     } else {
                         // 회원 가입 실패
                         Log.d("SignupDebug", "회원 가입 실패: code = $code")
