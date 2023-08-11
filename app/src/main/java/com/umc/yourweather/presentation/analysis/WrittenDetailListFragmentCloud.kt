ba21d1a82e366944c4dfc2f188c32a506d4b2ab7
kangyuri1114 <yuri3334@gmail.com>
2023-08-12 03:18:55 +0900

    [refactor] : 현재의 달, 그에 맞는 통계 리스트

--- app/src/main/java/com/umc/yourweather/presentation/analysis/WrittenDetailListFragmentCloud.kt
+++ app/src/main/java/com/umc/yourweather/presentation/analysis/WrittenDetailListFragmentCloud.kt
@@ -1,6 +1,7 @@
 package com.umc.yourweather.presentation.analysis
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,21 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.yourweather.R
 import com.umc.yourweather.data.entity.ItemWritten
+import com.umc.yourweather.data.remote.response.BaseResponse
+import com.umc.yourweather.data.remote.response.StatisticResponse
+import com.umc.yourweather.data.service.ReportService
 import com.umc.yourweather.databinding.FragmentWrittenDetailListCloudBinding
+import com.umc.yourweather.di.RetrofitImpl
 import com.umc.yourweather.presentation.adapter.WrittenRVAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.Calendar
 
 class WrittenDetailListFragmentCloud : Fragment() {
     private var _binding: FragmentWrittenDetailListCloudBinding? = null
     private val binding get() = _binding!!
+    private var currentMonth = monthGenerator()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,11 +55,54 @@ class WrittenDetailListFragmentCloud : Fragment() {
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+
+        // 인자(bundle)로부터 ago 값을 가져오기
+        val ago = arguments?.getInt("ago", 0) ?: 0
+        iconStatisticsMonthApi(ago)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    // 이번 달 통계
+    private fun iconStatisticsMonthApi(ago: Int) {
+        val service = RetrofitImpl.authenticatedRetrofit.create(ReportService::class.java)
+        val call = service.monthlyStatistic(ago = ago)
+
+        // 로그로 확인하기 위한 언제 달인지 변수
+        var viewMonth = currentMonth - ago
+        binding.tvWrittenDetailListMonthCloud.text = "${viewMonth}월 구름 약간 통계"
+
+        call.enqueue(object : Callback<BaseResponse<StatisticResponse>> {
+            override fun onResponse(
+                call: Call<BaseResponse<StatisticResponse>>,
+                response: Response<BaseResponse<StatisticResponse>>,
+            ) {
+                if (response.isSuccessful) {
+                    val statisticResponse = response.body()?.result // 'data'가 실제 응답 데이터를 담고 있는 필드일 경우
+                    if (statisticResponse != null) {
+                        Log.d("${ago}개월 전 ${viewMonth}월 Success", "${viewMonth}월 디테일 cloudy: ${statisticResponse.cloudy}")
+                        binding.tvWrittenDetailListMonthContent.text = "구름 약간이 ${viewMonth}월 전체 날씨의 ${statisticResponse.cloudy.toInt()}%를 차지했어요."
+
+                    } else {
+                        Log.e("${ago}개월 전 디테일 API Error", "Response body 비었음")
+                    }
+                } else {
+                    val errorBody = response.errorBody()?.string()
+                    Log.e("${ago}개월 전 디테일 API Error", "Response Code: ${response.code()}, Error Body: $errorBody")
+                }
+            }
+
+            override fun onFailure(call: Call<BaseResponse<StatisticResponse>>, t: Throwable) {
+                Log.e("${ago}개월 전 디테일 bar API Failure", "Error: ${t.message}", t)
+            }
+        })
+    }
+
+    fun monthGenerator(): Int {
+        val instance = Calendar.getInstance()
+        var month = (instance.get(Calendar.MONTH) + 1)
+        // var week = instance.get(Calendar.WEEK_OF_MONTH).toString()
+        Log.d("TimeGenerator", "Current Date: $month")
+
+        return month
     }
 
     private fun navigateToAnalysisFragment() {
@@ -69,5 +122,10 @@ class WrittenDetailListFragmentCloud : Fragment() {
 
 
         return dataList
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
