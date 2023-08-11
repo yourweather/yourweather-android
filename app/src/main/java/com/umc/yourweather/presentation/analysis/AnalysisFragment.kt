256da42145c357e9d4265b0a94b3f0a62dc008b1
kangyuri1114 <yuri3334@gmail.com>
2023-08-12 00:06:18 +0900

    [feat] : 미입력 내역이 있는/없는 경우 벨 이미지 변환, 각 경우에 이어질 화면 붙이기

--- app/src/main/java/com/umc/yourweather/presentation/analysis/AnalysisFragment.kt
+++ app/src/main/java/com/umc/yourweather/presentation/analysis/AnalysisFragment.kt
@@ -3,13 +3,21 @@ package com.umc.yourweather.presentation.analysis
 import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.umc.yourweather.R
 import com.umc.yourweather.R.drawable
+import com.umc.yourweather.data.remote.response.BaseResponse
+import com.umc.yourweather.data.remote.response.MissedInputResponse
+import com.umc.yourweather.data.service.ReportService
 import com.umc.yourweather.databinding.FragmentAnalysisBinding
+import com.umc.yourweather.di.RetrofitImpl
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 @Suppress("DEPRECATION")
 class AnalysisFragment : Fragment() {
@@ -81,13 +89,17 @@ class AnalysisFragment : Fragment() {
             }
         }
 
-        binding.btnBell.setOnClickListener {
-            val mFragment = AllWrittenFragment()
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fl_content, mFragment)
-                .addToBackStack(null)
-                .commit()
-        }
+        /**
+         * binding.btnBell.setOnClickListener {
+         val mFragment = AllWrittenFragment()
+         requireActivity().supportFragmentManager.beginTransaction()
+         .replace(R.id.fl_content, mFragment)
+         .addToBackStack(null)
+         .commit()
+         }
+         * */
+
+        unWrittenApi()
     }
 
     private fun viewMonthly() {
@@ -104,6 +116,54 @@ class AnalysisFragment : Fragment() {
         transaction.replace(R.id.f_iconStatic, IconStaticsWeeklyFragment())
         transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    private fun unWrittenApi() {
+        val service = RetrofitImpl.authenticatedRetrofit.create(ReportService::class.java)
+        val call = service.noInput()
+
+        call.enqueue(object : Callback<BaseResponse<MissedInputResponse>> {
+            override fun onResponse(
+                call: Call<BaseResponse<MissedInputResponse>>,
+                response: Response<BaseResponse<MissedInputResponse>>,
+            ) {
+                if (response.isSuccessful) {
+                    val missedInputResponse = response.body()?.result // 'data'가 실제 응답 데이터를 담고 있는 필드일 경우
+                    if (missedInputResponse != null) {
+                        val localDates = missedInputResponse.localDates
+                        Log.d("Success", "localDates: $localDates")
+
+                        if (localDates == null) {
+                            binding.btnBell.setOnClickListener {
+                                val mFragment = AllWrittenFragment()
+                                requireActivity().supportFragmentManager.beginTransaction()
+                                    .replace(R.id.fl_content, mFragment)
+                                    .addToBackStack(null)
+                                    .commit()
+                            }
+                        } else {
+                            binding.imgBellEvent.visibility = View.VISIBLE
+                            binding.btnBell.setOnClickListener {
+                                val mFragment = UnwrittenDetailListFragment()
+                                requireActivity().supportFragmentManager.beginTransaction()
+                                    .replace(R.id.fl_content, mFragment)
+                                    .addToBackStack(null)
+                                    .commit()
+                            }
+                        }
+                    } else {
+                        Log.e("Success (null)", "Response body 비었음")
+                    }
+                } else {
+                    val errorBody = response.errorBody()?.string()
+                    Log.e("API Error", "Response Code: ${response.code()}, Error Body: $errorBody")
+                }
+            }
+
+            override fun onFailure(call: Call<BaseResponse<MissedInputResponse>>, t: Throwable) {
+                Log.e("API Failure", "Error: ${t.message}", t)
+            }
+        })
     }
 
     override fun onDestroyView() {
