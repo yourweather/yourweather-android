35e424e32627f6bafd804f9c6d739b3a3fa1518b
kangyuri1114 <yuri3334@gmail.com>
2023-08-12 03:08:47 +0900

    [remove] : 필요 없는 파일 삭제

--- app/src/main/java/com/umc/yourweather/data/service/WeatherDataListener.kt
+++
@@ -1,5 +0,0 @@
-package com.umc.yourweather.data.service
-
-interface WeatherDataListener {
-    fun onWeatherDataReceived(ago: Int, sunnyPercent: Int, cloudyPercent: Int, rainyPercent: Int, lightningPercent: Int)
-}
