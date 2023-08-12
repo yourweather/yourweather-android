f8d9ae068a9e7ac3dc3e7741e2185c02a3513e68
Parkseojin <seojin5565@gmail.com>
2023-08-12 16:40:24 +0900

    [remove] : getMonthData 세팅 #230

--- app/src/main/java/com/umc/yourweather/data/service/WeatherService.kt
+++ app/src/main/java/com/umc/yourweather/data/service/WeatherService.kt
@@ -1,13 +1,17 @@
 package com.umc.yourweather.data.service
 
+import com.umc.yourweather.data.enums.Status
 import com.umc.yourweather.data.remote.response.BaseResponse
 import com.umc.yourweather.data.remote.response.HomeResponse
 import com.umc.yourweather.data.remote.response.MissedInputResponse
+import com.umc.yourweather.data.remote.response.MonthResponse
 import com.umc.yourweather.data.remote.response.WeatherResponse
 import retrofit2.Call
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
+import java.time.Month
 
 interface WeatherService {
     // 날씨 미입력 조회
@@ -18,7 +22,14 @@ interface WeatherService {
     @GET("/api/v1/weather/home")
     fun getHomeData(): Call<BaseResponse<HomeResponse>>
 
-    // 날씨 삭제
+    // 월별 데이터 조회
+    @GET("/api/v1/weather/monthly/{year}/{month}")
+    fun getMonthData(
+        @Query("year") year: Int,
+        @Query("month") month: Int,
+    ): Call<BaseResponse<MonthResponse>>
+
+    // 월별 조회
     @DELETE("/api/v1/weather/{year}-{month}-{day}")
     fun deleteWeather(
         @Path("year") year: Int,
