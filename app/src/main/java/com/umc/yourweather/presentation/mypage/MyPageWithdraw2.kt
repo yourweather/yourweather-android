07f1d2afa867941ba0a42b70b84139862455204f
Parkseojin <seojin5565@gmail.com>
2023-08-14 06:56:47 +0900

    [feat] : 회원탈퇴 구현완 #313

--- app/src/main/java/com/umc/yourweather/presentation/mypage/MyPageWithdraw2.kt
+++ app/src/main/java/com/umc/yourweather/presentation/mypage/MyPageWithdraw2.kt
@@ -1,53 +1,80 @@
 package com.umc.yourweather.presentation.mypage
 
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import com.umc.yourweather.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.data.remote.response.BaseResponse
+import com.umc.yourweather.data.remote.response.UserResponse
+import com.umc.yourweather.data.service.UserService
 import com.umc.yourweather.databinding.ActivityMyPageWithdraw2Binding
+import com.umc.yourweather.di.RetrofitImpl
+import com.umc.yourweather.util.AlertDialogTwoBtn
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyPageWithdraw2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMyPageWithdraw2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_page_withdraw2)
-
-        // 바인딩 객체 초기화
         binding = ActivityMyPageWithdraw2Binding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        // 프래그먼트를 추가하고 초기화
-        val fragmentManager: FragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-
-        // 프래그먼트 객체 생성
-        val fragment: Fragment = WithdrawPageFragment()
-
-        // 프래그먼트를 레이아웃 컨테이너에 추가
-        fragmentTransaction.add(R.id.fragment_container, fragment)
-
-
-        // 프래그먼트 트랜잭션 완료
-        fragmentTransaction.commit()
-
-        val radioButton1 = binding.radioButton1
-        val radioButton2 = binding.radioButton2
-        val radioButton3 = binding.radioButton3
-        val radioButton4 = binding.radioButton4
-        val radioButton5 = binding.radioButton5
-
-        val btnCancel: Button = findViewById(R.id.btn_choiceCancel)
-
-        btnCancel.setOnClickListener {
-            val intent = Intent(this@MyPageWithdraw2, MyPageWithdraw::class.java)
-            startActivity(intent)
+        binding.btnWithdraw2Withdraw.setOnClickListener {
+            AlertDialogTwoBtn(this).run {
+                setNegativeButton(
+                    "취소",
+                    object : DialogInterface.OnClickListener {
+                        override fun onClick(p0: DialogInterface?, p1: Int) {
+                            finish()
+                        }
+                    },
+                )
+                setPositiveButton(
+                    "탈퇴하기",
+                    object : DialogInterface.OnClickListener {
+                        override fun onClick(p0: DialogInterface?, p1: Int) {
+                            withdrawApi()
+                        }
+                    },
+                )
+                show()
+            }
         }
+        binding.btnWithdraw2Withdraw
+    }
 
+//    @PUT("/api/v1/users/withdraw")
+//    fun withdrawUser(): Call<BaseResponse<UserResponse>>
+    fun withdrawApi() {
+        val service = RetrofitImpl.authenticatedRetrofit.create(UserService::class.java)
+
+        service.withdrawUser().enqueue(object : Callback<BaseResponse<UserResponse>> {
+            override fun onResponse(
+                call: Call<BaseResponse<UserResponse>>,
+                response: Response<BaseResponse<UserResponse>>,
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+
+                    val code = responseBody?.code
+                    if (code == 200) {
+                        Log.d("WithDrawDebug", "회원탈퇴 성공")
+                        val intent = Intent(this@MyPageWithdraw2, MyPageWithdarw3::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                } else {
+                    Log.d("WithDrawDebug", "onResponse")
+                }
+            }
+
+            override fun onFailure(call: Call<BaseResponse<UserResponse>>, t: Throwable) {
+                Log.d("WithDrawDebug", "onFailure")
+            }
+        })
     }
 }
