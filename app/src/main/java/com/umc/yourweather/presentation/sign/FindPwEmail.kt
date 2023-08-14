5d97f49d7fbec9c210148470e9d22243624b59f5
Parkseojin <seojin5565@gmail.com>
2023-08-14 23:58:52 +0900

    [style] : ktlint 적용 #302

--- app/src/main/java/com/umc/yourweather/presentation/sign/FindPwEmail.kt
+++ app/src/main/java/com/umc/yourweather/presentation/sign/FindPwEmail.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.view.Window
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.navercorp.nid.oauth.NidOAuthPreferencesManager.code
