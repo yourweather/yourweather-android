10e16834597c5e7d681b7f5435a7ca7725a2b388
Yerin Song <angelrin7@naver.com>
2023-08-15 03:15:42 +0900

    [feat] : 이용약관, 개인정보 페이지에서 뒤로가기 기능 구현

--- app/src/main/java/com/umc/yourweather/presentation/mypage/MyPagePrivacyPolicy.kt
+++ app/src/main/java/com/umc/yourweather/presentation/mypage/MyPagePrivacyPolicy.kt
@@ -1,4 +1,24 @@
 package com.umc.yourweather.presentation.mypage
 
-class MyPagePrivacyPolicy {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.databinding.ActivityMyPagePrivacyPolicyBinding
+
+class MyPagePrivacyPolicy : AppCompatActivity() {
+    lateinit var binding: ActivityMyPagePrivacyPolicyBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyPagePrivacyPolicyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 버튼 클릭으로 되돌아가기
+        binding.btnPrivacyPolicyBack.setOnClickListener {
+            finish()
+        }
+        // 영역 클릭으로 되돌아가기
+        binding.flPrivacyPolicyBack.setOnClickListener {
+            finish()
+        }
+    }
 }
--- app/src/main/java/com/umc/yourweather/presentation/mypage/MyPageUsePolicy.kt
+++ app/src/main/java/com/umc/yourweather/presentation/mypage/MyPageUsePolicy.kt
@@ -1,4 +1,24 @@
 package com.umc.yourweather.presentation.mypage
 
-class MyPageUsePolicy {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.databinding.ActivityMyPageUsePolicyBinding
+
+class MyPageUsePolicy : AppCompatActivity() {
+    lateinit var binding: ActivityMyPageUsePolicyBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyPageUsePolicyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 버튼 클릭으로 되돌아가기
+        binding.btnUsePolicyBack.setOnClickListener {
+            finish() 
+        }
+        // 영역 클릭으로 되돌아가기
+        binding.flUsePolicyBack.setOnClickListener {
+            finish() 
+        }
+    }
 }
