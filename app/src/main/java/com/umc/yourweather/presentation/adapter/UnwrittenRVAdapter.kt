653aef00ba5a320c986b3111f8de66d0b8074426
kangyuri1114 <yuri3334@gmail.com>
2023-08-16 02:05:50 +0900

    [feat] : 미입력 내역 날짜 CalendarDetailviewModify2로 전달

--- app/src/main/java/com/umc/yourweather/presentation/adapter/UnwrittenRVAdapter.kt
+++ app/src/main/java/com/umc/yourweather/presentation/adapter/UnwrittenRVAdapter.kt
@@ -2,14 +2,16 @@ package com.umc.yourweather.presentation.adapter
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.umc.yourweather.data.entity.ItemUnwritten
 import com.umc.yourweather.databinding.ItemUnwrittenDetailBinding
 import com.umc.yourweather.presentation.calendar.CalendarTotalViewFragment
+import com.umc.yourweather.presentation.calendardetailview.CalendarDetailviewModify2
 
-class UnwrittenRVAdapter(private val dataList: List<ItemUnwritten>, private val context: Context) :
+class UnwrittenRVAdapter(private val dataList: List<ItemUnwritten>, private val localDates: List<String>, private val context: Context) :
     RecyclerView.Adapter<UnwrittenRVAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -30,15 +32,22 @@ class UnwrittenRVAdapter(private val dataList: List<ItemUnwritten>, private val 
         return dataList.size
     }
 
+    @Suppress("DEPRECATION")
     inner class MyViewHolder(val binding: ItemUnwrittenDetailBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(data: ItemUnwritten) {
             binding.tvUnwrittenDetail.text = data.formattedDate
+            val position = adapterPosition
+            val localDate = localDates[position] // 해당 아이템의 localDate 가져오기
 
-            binding.btnStaticsRightDetail1.setOnClickListener {
-                val intent = Intent(context, CalendarTotalViewFragment::class.java)
+            binding.linearLayout3.setOnClickListener {
+                val intent = Intent(context, CalendarDetailviewModify2::class.java)
                 context.startActivity(intent)
+                val dIntent = Intent(context, CalendarDetailviewModify2::class.java)
+                dIntent.putExtra("unWrittenDate", localDate)
+                context.startActivity(dIntent)
+                Log.d("localDate","$localDate")
             }
         }
     }
