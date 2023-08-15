71a6f1b20fba8e0cff6a62acceb804a9b55a7f0f
kangyuri1114 <yuri3334@gmail.com>
2023-08-16 03:15:31 +0900

    [feat] : API 함수 구현

--- app/src/main/java/com/umc/yourweather/presentation/calendardetailview/CalendarDetailviewModify2.kt
+++ app/src/main/java/com/umc/yourweather/presentation/calendardetailview/CalendarDetailviewModify2.kt
@@ -3,25 +3,30 @@ package com.umc.yourweather.presentation.calendardetailview
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
+import android.util.Log
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.SeekBar
-import android.widget.TextView
 import android.widget.TimePicker
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import com.umc.yourweather.R
+import com.umc.yourweather.data.enums.Status
+import com.umc.yourweather.data.remote.request.MemoRequest
+import com.umc.yourweather.data.remote.response.BaseResponse
+import com.umc.yourweather.data.remote.response.MemoResponse
+import com.umc.yourweather.data.service.MemoService
 import com.umc.yourweather.databinding.ActivityCalendarDetailviewModify2Binding
+import com.umc.yourweather.di.RetrofitImpl
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class CalendarDetailviewModify2 : AppCompatActivity() {
     private lateinit var binding: ActivityCalendarDetailviewModify2Binding
     private lateinit var cardView: CardView
@@ -46,13 +51,50 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
         // seekBar 보여죽기
         setupSeekBarListener()
         // 카드뷰 보여주기
-        setupCardViewClickListener()
+        cardView.setOnClickListener {
+            showKeyboardAndFocusEditText()
+        }
         // 날씨 아이콘 보여주기
         setupWeatherButtons()
         // 시간 선택 창
         setupTimePicker()
+
+        // 미입력에서 넘어오는 날짜
+        val unWrittenDate = intent.getStringExtra("unWrittenDate")
+        if (unWrittenDate != null) {
+            val dateParts = unWrittenDate.split("-")
+            if (dateParts.size == 3) {
+                val year = dateParts[0].toInt()
+                val month = dateParts[1].toInt()
+                val day = dateParts[2].toInt()
+
+                binding.tvDetailviewModify2Date.text = "${month}월 ${day}의 기록"
+            }
+        }
     }
 
+
+    private fun writeMemo(status: Status, content: String?, localDateTime: String?, temperature: Int?) {
+        val MemoService = RetrofitImpl.authenticatedRetrofit.create(MemoService::class.java)
+        MemoService.memoWrite(MemoRequest(status, content, localDateTime, temperature))
+            .enqueue(object : Callback<BaseResponse<MemoResponse>> {
+                override fun onResponse(
+                    call: Call<BaseResponse<MemoResponse>>,
+                    response: Response<BaseResponse<MemoResponse>>,
+                ) {
+                    if (response.isSuccessful) {
+                        val MemoResponse = response.body()?.result
+                        Log.d("메모 작성", "메모 작성, 전달 성공 ${response.body()?.result}")
+                    } else {
+                        Log.d("메모 작성 실패", "메모 작성, 전달 성공 실패: ${response.code()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<BaseResponse<MemoResponse>>, t: Throwable) {
+                    Log.d("메모 작성 요청", "메모 작성 요청 실패: ${t.message}")
+                }
+            })
+    }
     private fun setupSeekBarListener() {
         val seekBar = binding.seekbarCalendarDetailviewTemp2
 
@@ -70,12 +112,6 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
                 // 필요한 경우 구현
             }
         })
-    }
-
-    private fun setupCardViewClickListener() {
-        cardView.setOnClickListener {
-            showKeyboardAndFocusEditText()
-        }
     }
 
     private fun setupWeatherButtons() {
@@ -101,6 +137,7 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
         }
     }
 
+    // 날씨 아이콘 애니메이션
     private fun animateAndHandleButtonClick(button: Button) {
         val buttonAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.btn_weather_scale)
 
@@ -112,12 +149,14 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
         button.startAnimation(buttonAnimation)
     }
 
+    // 버튼 활성화
     private fun updateButtonInFragmentColor(colorId: Int) {
         val fragment1: Fragment = ModifyFragment2()
         val buttonInFragment = fragment1.view?.findViewById<Button>(R.id.btn_calendardetailview_save)
         buttonInFragment?.setTextColor(ContextCompat.getColor(this, colorId))
     }
 
+    // 시간 선택 창
     private fun setupTimePicker() {
         val timePicker: TimePicker = findViewById(R.id.tp_calendardetailview)
 
@@ -139,16 +178,8 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
             "오전"
         }
 
-        val tvTime: TextView = findViewById(R.id.tv_calendar_detailview_modify2_time)
-        tvTime.text = ("$selectedAmPm $selectedHour:${selectedMinute}시")
-    }
-
-    // 미입력 내역에서 넘어오는 date 값 처리하는 로직 추가
-    private fun handleUnwrittenDate() {
-        val unWrittenDate = intent.getStringExtra("unWrittenDate")
-        if (unWrittenDate != null) {
-            // receivedDate를 활용하여 처리하는 로직을 작성하세요
-        }
+//        val tvTime: TextView = findViewById(R.id.tv_calendar_detailview_modify2_time)
+//        tvTime.text = ("$selectedAmPm $selectedHour:${selectedMinute}시")
     }
 
     private fun updateSaveButtonState() {
