893d932ce0d839bbd29573b5371c1ad1a2aedbd3
Yerin Song <angelrin7@naver.com>
2023-08-15 23:12:26 +0900

    [add] : 캘린더상세보기 관련 파일

--- app/src/main/java/com/umc/yourweather/presentation/adapter/CalendarDetailviewDiaryAdapter.kt
+++ app/src/main/java/com/umc/yourweather/presentation/adapter/CalendarDetailviewDiaryAdapter.kt
@@ -8,8 +8,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.umc.yourweather.R
+import com.umc.yourweather.data.remote.response.MemoDailyResponse
 
-class CalendarDetailviewDiaryAdapter():
+class CalendarDetailviewDiaryAdapter(private val memoContentList: List<MemoDailyResponse.MemoContentResponse>):
     RecyclerView.Adapter<CalendarDetailviewDiaryAdapter.MyViewHolder>() {
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         public var itemdetail: TextView = itemView.findViewById(R.id.tv_calendar_detailview_diary)
@@ -31,8 +32,8 @@ class CalendarDetailviewDiaryAdapter():
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-
-        holder.itemdetail.setText("세부사항 입니다")
+        val memoContent = memoContentList[position] // Get the MemoContentResponse at the current position
+        holder.itemdetail.text = memoContent.status
     }
 
     // 3. Return the size of your dataset (invoked by the layout manager)
