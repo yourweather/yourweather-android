ccb1f014232f38172ea35f1aacf82f81e682c806
kangyuri1114 <yuri3334@gmail.com>
2023-08-16 03:32:21 +0900

    [feat] : 메모 작성 후 POST 성공

--- app/src/main/java/com/umc/yourweather/presentation/calendardetailview/CalendarDetailviewModify2.kt
+++ app/src/main/java/com/umc/yourweather/presentation/calendardetailview/CalendarDetailviewModify2.kt
@@ -26,6 +26,9 @@ import com.umc.yourweather.di.RetrofitImpl
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class CalendarDetailviewModify2 : AppCompatActivity() {
     private lateinit var binding: ActivityCalendarDetailviewModify2Binding
@@ -57,7 +60,7 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
         // 날씨 아이콘 보여주기
         setupWeatherButtons()
         // 시간 선택 창
-        setupTimePicker()
+       // setupTimePicker()
 
         // 미입력에서 넘어오는 날짜
         val unWrittenDate = intent.getStringExtra("unWrittenDate")
@@ -71,9 +74,28 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
                 binding.tvDetailviewModify2Date.text = "${month}월 ${day}의 기록"
             }
         }
+
+//
+//        val status: Status = Status.YOUR_STATUS // Status 열거형에 맞게 설정
+//        val content: String? = "메모 내용"
+//        val localDateTime: String? = "2023-08-16T12:34:56" // 예시 날짜와 시간
+//        val temperature: Int? = 25
+//
+//        // writeMemo 함수 호출
+//        writeMemo(status, content, localDateTime, temperature)
+
+        binding.btnCalendardetailviewSave.setOnClickListener {
+            val status: Status = selectedStatus // Status 열거형에 맞게 설정
+            val content: String? = editText.text?.toString() // editText에서 입력한 내용 가져오기
+            val localDateTime: String? = getCurrentDateTime() // 저장 버튼 누른 시간
+            val temperature: Int? = binding.seekbarCalendarDetailviewTemp2.progress // seekBar의 진행 상태 가져오기
+
+            // writeMemo 함수 호출
+            writeMemo(status, content, localDateTime, temperature)
+        }
     }
 
-
+    // 메모 요청 API
     private fun writeMemo(status: Status, content: String?, localDateTime: String?, temperature: Int?) {
         val MemoService = RetrofitImpl.authenticatedRetrofit.create(MemoService::class.java)
         MemoService.memoWrite(MemoRequest(status, content, localDateTime, temperature))
@@ -95,6 +117,13 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
                 }
             })
     }
+
+    // 현재 시간을 가져오는 함수
+    private fun getCurrentDateTime(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+        val now = Calendar.getInstance().time
+        return dateFormat.format(now)
+    }
     private fun setupSeekBarListener() {
         val seekBar = binding.seekbarCalendarDetailviewTemp2
 
@@ -114,24 +143,30 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
         })
     }
 
+    private var selectedStatus: Status = Status.SUNNY // 기본값으로 SUNNY 설정
+
     private fun setupWeatherButtons() {
         val buttonAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.btn_weather_scale)
 
         binding.btnHomeSun.setOnClickListener {
+            selectedStatus = Status.SUNNY
             animateAndHandleButtonClick(binding.btnHomeCloud)
         }
 
         binding.btnHomeCloud.setOnClickListener {
+            selectedStatus = Status.CLOUDY
             animateAndHandleButtonClick(binding.btnHomeCloud)
             updateButtonInFragmentColor(R.color.sorange)
         }
 
         binding.btnHomeThunder.setOnClickListener {
+            selectedStatus = Status.LIGHTNING
             animateAndHandleButtonClick(binding.btnHomeCloud)
             updateButtonInFragmentColor(R.color.sorange)
         }
 
         binding.btnHomeRain.setOnClickListener {
+            selectedStatus = Status.RAINY
             animateAndHandleButtonClick(binding.btnHomeCloud)
             updateButtonInFragmentColor(R.color.sorange)
         }
@@ -156,31 +191,31 @@ class CalendarDetailviewModify2 : AppCompatActivity() {
         buttonInFragment?.setTextColor(ContextCompat.getColor(this, colorId))
     }
 
-    // 시간 선택 창
-    private fun setupTimePicker() {
-        val timePicker: TimePicker = findViewById(R.id.tp_calendardetailview)
-
-        val selectedHour: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            timePicker.hour
-        } else {
-            timePicker.currentHour
-        }
-
-        val selectedMinute: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            timePicker.minute
-        } else {
-            timePicker.currentMinute
-        }
-
-        val selectedAmPm: String = if (selectedHour >= 12) {
-            "오후"
-        } else {
-            "오전"
-        }
-
-//        val tvTime: TextView = findViewById(R.id.tv_calendar_detailview_modify2_time)
-//        tvTime.text = ("$selectedAmPm $selectedHour:${selectedMinute}시")
-    }
+//    // 시간 선택 창
+//    private fun setupTimePicker() {
+//        val timePicker: TimePicker = findViewById(R.id.tp_calendardetailview)
+//
+//        val selectedHour: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            timePicker.hour
+//        } else {
+//            timePicker.currentHour
+//        }
+//
+//        val selectedMinute: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            timePicker.minute
+//        } else {
+//            timePicker.currentMinute
+//        }
+//
+//        val selectedAmPm: String = if (selectedHour >= 12) {
+//            "오후"
+//        } else {
+//            "오전"
+//        }
+//
+////        val tvTime: TextView = findViewById(R.id.tv_calendar_detailview_modify2_time)
+////        tvTime.text = ("$selectedAmPm $selectedHour:${selectedMinute}시")
+//    }
 
     private fun updateSaveButtonState() {
         val fragment1: Fragment? = supportFragmentManager.findFragmentById(R.id.fragment_container)
