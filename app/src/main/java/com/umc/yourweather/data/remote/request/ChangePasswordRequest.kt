0f7bb51ef7b2c57ff5b3a5146874ee1f62adc9c8
kangyuri1114 <yuri3334@gmail.com>
2023-08-15 13:04:52 +0900

    [add] : 변경된 API에 필요한 응답, 요청 파일 추가

--- app/src/main/java/com/umc/yourweather/data/remote/request/ChangePasswordRequest.kt
+++ app/src/main/java/com/umc/yourweather/data/remote/request/ChangePasswordRequest.kt
@@ -5,4 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class ChangePasswordRequest(
     @SerializedName("password")
     val password: String,
+    @SerializedName("newPassword")
+    val newPassword: String
 )
