a5295c2a693bee82662d2a39a2e465128949060e
Parkseojin <seojin5565@gmail.com>
2023-08-18 05:07:20 +0900

    [feat] : 그래프 되긴됨 #369

--- app/src/main/java/com/umc/yourweather/presentation/calendar/CalendarDetail.kt
+++ app/src/main/java/com/umc/yourweather/presentation/calendar/CalendarDetail.kt
@@ -1,17 +1,19 @@
 package com.umc.yourweather.presentation.calendar
 
-import android.R
-import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.DefaultValueFormatter
+import com.umc.yourweather.R
 import com.umc.yourweather.data.remote.response.BaseResponse
 import com.umc.yourweather.data.remote.response.MemoDailyResponse
 import com.umc.yourweather.data.service.MemoService
@@ -20,7 +22,6 @@ import com.umc.yourweather.di.RetrofitImpl
 import com.umc.yourweather.di.UserSharedPreferences
 import com.umc.yourweather.presentation.adapter.CalendarDetailMemoContentAdapter
 import com.umc.yourweather.presentation.adapter.CalendarDetailMemoListAdapter
-import com.umc.yourweather.presentation.calendardetailview.CalendarDetailviewModify1
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -51,27 +52,6 @@ class CalendarDetail : AppCompatActivity() {
             emptyView(thisDate)
         } else {
             CalendarDetailViewApi(weatherId, thisDate)
-        }
-
-        val toolbar: Toolbar = binding.toolbar
-        supportActionBar?.title = "My Toolbar"
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val menuInflater = menuInflater
-        menuInflater.inflate(com.umc.yourweather.R.menu.memu_appbar_back, menu)
-        return true
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                finish()
-                return true
-            }
-
-            else -> return super.onOptionsItemSelected(item)
         }
     }
 
@@ -157,16 +137,18 @@ class CalendarDetail : AppCompatActivity() {
 
             binding.llCalendarDetailNoMemo.setOnClickListener {
                 // 지금 입력하기 누름
-                // 입력창 이동
-                val intent = Intent(this, CalendarDetailviewModify1::class.java)
-                startActivity(intent)
             }
+
+            // 차트
+            chart(memoList)
 
             memoListView(memoList)
         } else if (memoList?.size!! > 0 && memoContent?.size!! > 0) {
             // 둘 다 있다.
             Log.d("calendarDetail memoContent 확인...", "$memoContent")
             memoListView(memoList)
+
+            chart(memoList)
 
             if (memoContent != null) {
                 memoContentView(memoContent)
@@ -205,15 +187,80 @@ class CalendarDetail : AppCompatActivity() {
 
         binding.llCalendarDetailNoTotalData.setOnClickListener {
             // 입력창 이동
-                val intent = Intent(this, CalendarDetailviewModify1::class.java)
-                startActivity(intent)
         }
 
+        binding.nsCalendarDetail.visibility = View.INVISIBLE
         binding.tvCalendarDetailTitle.visibility = View.INVISIBLE
         binding.tvCalendarDetailMemoTitle.visibility = View.INVISIBLE
-        binding.llCalendarDetailRcy.visibility = View.INVISIBLE
+//        binding.llCalendarDetailRcy.visibility = View.INVISIBLE
         binding.dividerCalendarDetailMemo.visibility = View.INVISIBLE
         binding.tvCalendarDetailMemoTitle.visibility = View.INVISIBLE
         binding.rvCalendarDetailMemocontent.visibility = View.INVISIBLE
     }
+
+    private fun chart(memoList: List<MemoDailyResponse.MemoItemResponse>) {
+        var temprList: MutableList<TemprData> = mutableListOf()
+        val linechart = binding.chartCalendarDetail
+
+        // val linechart = findViewById<LineChart>(R.id.line_chart)
+
+        val xAxis = linechart.xAxis
+        for (i in 0 until memoList.size) {
+            temprList.add(TemprData(memoList[i].creationDatetime, memoList[i].temperature))
+        }
+
+        val entries: MutableList<Entry> = mutableListOf()
+
+        for (i in temprList.indices) {
+            entries.add(Entry(i.toFloat(), temprList[i].tempr.toFloat()))
+        }
+
+        val lineDataSet = LineDataSet(entries, "entries")
+
+        lineDataSet.apply {
+            color = resources.getColor(R.color.black, null)
+            circleRadius = 5f
+            lineWidth = 3f
+            setCircleColor(resources.getColor(R.color.gray, null))
+            circleHoleColor = resources.getColor(R.color.white, null)
+            setDrawHighlightIndicators(false)
+            // setDrawValues(true) // 숫자표시
+            // #F0A830
+            color = Color.parseColor("#F0A830")
+            valueTextColor = resources.getColor(R.color.black, null)
+            valueFormatter = DefaultValueFormatter(0) // 소숫점 자릿수 설정
+            valueTextSize = 10f
+        }
+
+        // 나머지 설정
+        linechart.apply {
+            axisRight.isEnabled = false // y축 사용여부
+            axisLeft.isEnabled = false
+            legend.isEnabled = false // legend 사용여부
+            description.isEnabled = false // 주석
+            //  isDragXEnabled = true   // x 축 드래그 여부
+            isScaleYEnabled = false // y축 줌 사용여부
+            isScaleXEnabled = false // x축 줌 사용여부
+        }
+
+        xAxis.apply {
+            isEnabled = false
+            setLabelCount(10, false)
+            setDrawGridLines(false)
+            setDrawAxisLine(true)
+            setDrawLabels(false)
+            position = XAxis.XAxisPosition.BOTTOM
+            textColor = resources.getColor(R.color.black, null)
+            textSize = 10f
+            labelRotationAngle = 0f
+            setLabelCount(10, true)
+            granularity = 95f
+        }
+        linechart.apply {
+            data = LineData(lineDataSet)
+            notifyDataSetChanged() // 데이터 갱신
+            invalidate() // view갱신
+        }
+    }
+    data class TemprData(val date: String, val tempr: Int)
 }
