f59e25d9f1ca0054ee7f5e10cd9b57979c89b0f7
kangyuri1114 <yuri3334@gmail.com>
2023-08-19 15:41:13 +0900

    [fix] : 편집 뷰 프래그먼트에서 액티비티로 전환

---
+++ app/src/main/java/com/umc/yourweather/presentation/calendardetailview/CalendarModifyWeatherActivity2.kt
@@ -0,0 +1,12 @@
+package com.umc.yourweather.presentation.calendardetailview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.yourweather.R
+
+class CalendarModifyWeatherActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar_modify_weather2)
+    }
+}
