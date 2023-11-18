26aca8e3d2d9cfd928466f7dddf131d49d92e22c
Parkseojin <seojin5565@gmail.com>
2023-11-19 02:19:43 +0900

    [add] : 이미 가입한 회원일 경우 뷰 #534

---
+++ app/src/main/java/com/umc/yourweather/presentation/sign/AlreadySignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.umc.yourweather.presentation.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umc.yourweather.R
+
+class AlreadySignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_already_sign_up)
+    }
+}
